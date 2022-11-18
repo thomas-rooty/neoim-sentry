66c0785fb09a6be71bdc907ba516430c14b52515
thomas-rooty <essainono1@gmail.com>
2022-11-18 14:18:12 +0100

    implemented AI in api

---
+++ pages/api/ia/[...param].tsx
@@ -0,0 +1,39 @@
+import type {NextApiRequest, NextApiResponse} from 'next'
+import * as tf from '@tensorflow/tfjs';
+
+type Data = {
+  stars: any[]
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // Catch all route
+  const {
+    query: {param},
+  } = req
+
+  // Define stars classes
+  const classes = ['Brown Dwarf', 'Red Dwarf', 'White Dwarf', 'Main Sequence', 'Super Giants', 'Hyper Giants']
+
+  // Load model
+  const model = await tf.loadLayersModel('model.json')
+
+  // Create a new array of features from the request
+  // @ts-ignore
+  const features = [param[0], param[1], param[2], param[3], param[4], param[5], param[6]]
+
+  // Make a prediction using the model
+  const prediction = model.predict(tf.tensor2d([features]))
+
+  // Get the index of the highest probability
+  //const index = prediction.argMax(1).dataSync()[0]
+  const index = (prediction as any).argMax(1).dataSync()[0]
+
+  // Get the class name
+  const starClass = classes[index]
+
+  // Return the class name
+  res.status(200).json({stars: [starClass]})
+}
